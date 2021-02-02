9dd2d8a4bdece90838c0a37e777b2e5f39616c0a
Liam Ganose <lganose@gmail.com>
2021-02-02 23:08:10 +0000

    Change file structure

---
+++ src/containers/App.jsx
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Holder for future homepage
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
